40177085cf2819f39170e0a825859f019c1b5131
Max Zakharzhevskiy <max.za@digitalpersona.com>
2019-11-20 21:06:48 -0800

    Initial w/ cheerio OK

---
+++ home.js
@@ -0,0 +1,8 @@
+import cheerio from 'cheerio';
+
+let $ = cheerio.load('<a></a>');
+
+console.log($.html());
+
+
+console.log('done');
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: './home',
+    output: {
+        filename: 'build.js'
+    },
+    devtool: 'source-map',
+    mode: 'development',
+};
