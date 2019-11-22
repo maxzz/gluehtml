08c12c5e3a0dd1f457a42a82f73ffb89e9afdcac
Max Zakharzhevskiy <max.za@digitalpersona.com>
2019-11-22 03:09:34 -0800

    More args checking

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import path from 'path';
 import fs from 'fs-extra';
 import minimist from 'minimist';
 
-function createSolidHtmlContent(filename: string): string {
-
-    let htmlString = fs.readFileSync(filename).toString();
-
+function createSolidHtmlContent(rootDir: string, htmlString: string): string {
     // 1. Parse HTML
     let $ = cheerio.load(htmlString);
 
@@ -18,12 +15,10 @@ function createSolidHtmlContent(filename: string): string {
     $('script').each((index, el) => externals.scripts.push({ url: el.attribs.src, el: el }));
 
     // 2. Load the content of externals
-    let parent = path.dirname(filename);
-    
     [...externals.links, ...externals.scripts].forEach(item => {
         try {
-            if (!item.rel || item.rel.trim().indexOf('stylesheet') !== -1) { // skip 'rel=icon'; handle =stylesheet and ="stylesheet"
-                item.cnt = fs.readFileSync(path.join(parent, item.url)).toString();
+            if (!item.rel || ~item.rel.trim().toLowerCase().indexOf('stylesheet')) { // skip 'rel=icon'; handle =stylesheet and ="stylesheet"
+                item.cnt = fs.readFileSync(path.join(rootDir, item.url)).toString();
             }
         } catch (err) {
             console.log(`Failed to read: '${item.url}'`, err);
@@ -42,26 +37,52 @@ function createSolidHtmlContent(filename: string): string {
     return $.html();
 }
 
+function createSolidHtml(filename: string): void {
+    filename = path.resolve(filename);
+    let rootDir = path.dirname(filename);
+
+    let htmlString = fs.readFileSync(filename).toString();
+    let newCnt = createSolidHtmlContent(rootDir, htmlString);
+
+    console.log(path.basename(filename, '.html'));
+    
+    let dest = path.join(rootDir, `${path.basename(filename, '.html')}${SUFFIX}${path.extname(filename)}`);
+    fs.writeFileSync(dest, newCnt);
+}
+
+var SUFFIX = '--single';
+
 function main() {
     const args = minimist(process.argv.slice(2));
 
-    let name = path.normalize(args.file);
+    let name = args.path || args['_'][0];
+    if (!name || !fs.existsSync(name)) {
+        console.log(`Provide HTML filename or folder name with HTML files.`);
+        process.exit(1);
+    }
+
+    args.suffix && typeof args.suffix === 'string' && (SUFFIX = args.suffix);
+
     try {
+        name = path.normalize(name);
         if (fs.statSync(name).isDirectory()) {
+            let dir = fs.readdirSync(name);
+            let names = dir
+                .filter(_ => path.extname(_) === '.html' && !~_.toLowerCase().indexOf(SUFFIX))
+                .map(_ => path.join(name, _));
+            names.forEach(createSolidHtml);
+            names || console.log(`No HTML files found in "${name}"`);
         }
         else {
-            name = path.resolve(name);
-            let newCnt = createSolidHtmlContent(name);
-            
-            let dest = path.join(path.dirname(name), path.basename(name) + '-single' + path.extname(name));
-            fs.writeFileSync(dest, newCnt);
+            createSolidHtml(name);
         }
     }
     catch (err) {
         console.log(err);
+        process.exit(2);
     }
 }
 
 main();
-console.log('done5');
+console.log('Done');
 
