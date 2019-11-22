fae5cfafb126928021654ea1ee5d5113ff7b6dcd
Max Zakharzhevskiy <max.za@digitalpersona.com>
2019-11-21 19:14:13 -0800

    Fixed ts matching regex

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import cheerio from 'cheerio';
+import path from 'path';
 
-let $ = cheerio.load('<a></a>');
+// let $ = cheerio.load('<a></a>');
+// console.log($.html());
 
-console.log($.html());
+function parseHtmlContent(orgHtml: string) {
+    let $ = cheerio.load(orgHtml);
+
+    let rv = {
+        links: [],
+        scripts: []
+    }
+
+    $('link').each((index, el) => rv.links.push(path.normalize(el.attribs.href)));
+    $('script').each((index, el) => rv.scripts.push(path.normalize(el.attribs.src)));
+
+    return rv;
+}
+
+console.log(parseHtmlContent('<a></a>'));
 
 
 console.log('done5');
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'build.js'
-    },
-    module: {
-        rules: [
-            {
-                test: '/\.ts$/',
-                use: ['ts-loader']
-            }
-        ]
-    },
-    devtool: 'source-map',
-    mode: 'development',
+module.exports = env => {
+
+    console.log(env);
+
+    return {
+        entry: './src/index.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'build.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader'
+                }
+            ]
+        },
+        devtool: 'source-map',
+        mode: 'development',
+    };
 };
